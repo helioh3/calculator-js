bb5aa5cfdb3e0fe5ebf43c3d611cd1eea8c73124
Helio Brito <helioh3@gmail.com>
2022-03-17 23:29:01 -0300

    calculator refactory 02

--- myscript.js
+++ myscript.js
@@ -7,29 +7,22 @@
   const $buttonCE = doc.querySelector('[data-js="button-ce"]')
   const $buttonEqual = doc.querySelector('[data-js="button-equal"]')
 
-  // console.log($buttonCE)
-  // $buttonsNumbers.forEach((value) => {
-  //   console.log(value)
-  // })
+  function initialize () {
+    initEvents()
+  }
 
-  /*$buttonsNumbers.forEach(function (button){
-    console.log(button)
-  })*/
+  function initEvents () {
+    Array.prototype.forEach.call($buttonsNumbers, (button) => {
+      button.addEventListener('click', handleClickNumber, false)
+    })
+    Array.prototype.forEach.call($buttonsOperations, function (button) {
+      button.addEventListener('click', handleClickOperation, false)
+    })
+    $buttonCE.addEventListener('click', handleClickCE, false)
+    $buttonEqual.addEventListener('click', handleClickEqual, false)
+  }
 
-  Array.prototype.forEach.call($buttonsNumbers, (button) => {
-    button.addEventListener('click', handleClickNumber, false)
-  })
-
-  Array.prototype.forEach.call($buttonsOperations, function (button) {
-    button.addEventListener('click', handleClickOperation, false)
-  })
-
-  $buttonCE.addEventListener('click', handleClickCE, false)
-
-  $buttonEqual.addEventListener('click', handleClickEqual, false)
-
-  function handleClickNumber (e) {
-
+  function handleClickNumber (event) {
     $visor.value += this.value
   }
 
@@ -38,49 +31,60 @@
     $visor.value += this.value
   }
 
-  function handleClickCE (e) {
+  function handleClickCE (event) {
     $visor.value = 0
   }
 
   function isLastItemOperation (number) {
-    const operations = ['+', '-', 'x', '÷', '=']
+    const operations = getOperations()
     let lastItem = number.split('').pop()
-
     return operations.some((operator) => {
       return operator === lastItem
     })
   }
 
-  function removeLastItemIsOperator (number) {
-    if (isLastItemOperation(number)) return number.slice(0, -1)
+  function getOperations () {
+    return Array.prototype.map.call($buttonsOperations, function (button) {
+      return button.value
+    })
+  }
 
-    return number
+  function removeLastItemIsOperator (string) {
+    if (isLastItemOperation(string)) return string.slice(0, -1)
+    return string
   }
 
   function handleClickEqual () {
     $visor.value = removeLastItemIsOperator($visor.value)
     const allValues = $visor.value.match(/\d+[+x÷-]?/g)
-    $visor.value = allValues.reduce(function (accumulated, current) {
-      const firstValue = accumulated.slice(0, -1)
-      const operator = accumulated.split('').pop()
-      const lastValue = removeLastItemIsOperator(current)
-      const lastOperator = isLastItemOperation(current) ? current.split('').pop() : ''
-      switch (operator) {
-        case '+' :
-          return (Number(firstValue) + Number(lastValue)) + lastOperator
-
-        case '-' :
-          return (Number(firstValue) - Number(lastValue)) + lastOperator
-
-        case 'x' :
-          return (Number(firstValue) * Number(lastValue)) + lastOperator
-
-        case '÷' :
-          return (Number(firstValue) / Number(lastValue)) + lastOperator
-      }
-
-    })
+    $visor.value = allValues.reduce(calculateAllValues)
   }
+
+  function calculateAllValues (accumulated, current) {
+    const firstValue = accumulated.slice(0, -1)
+    const operator = accumulated.split('').pop()
+    const lastValue = removeLastItemIsOperator(current)
+    const lastOperator = isLastItemOperation(current) ? current.split('').pop() : ''
+    return doOperation(operator, firstValue, lastValue) + lastOperator
+  }
+
+  function doOperation (operator, firstValue, lastValue) {
+    switch (operator) {
+      case '+' :
+        return Number(firstValue) + Number(lastValue)
+
+      case '-' :
+        return Number(firstValue) - Number(lastValue)
+
+      case 'x' :
+        return Number(firstValue) * Number(lastValue)
+
+      case '÷' :
+        return Number(firstValue) / Number(lastValue)
+    }
+  }
+
+  initialize()
 
   // function myFunction () {
   //   arguments.forEach(function (arg){
